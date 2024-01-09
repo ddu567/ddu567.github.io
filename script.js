6250d2fe82a6e5fb684ef459660a88a4a040b3e4
ddu567 <shbea0503@naver.com>
2024-01-09 23:23:52 +0900

    240109
    - works 내용 및 디자인 수정

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-
 const preview = document.querySelectorAll(".preview > img");
 const tabBtns = document.querySelectorAll(".tabTitle > div");
 const tabConts = document.querySelectorAll('.contentWrap > div');
 
+// preview click
+preview.forEach((preview, index) => {
+  preview.addEventListener('click', () => {
+    tabBtns.forEach(otherBtn => {
+      otherBtn.classList.remove('tabOn');
+    });
+    tabConts.forEach(othercont => {
+      othercont.classList.remove('contentOn');
+    });
+    tabBtns[index].classList.add('tabOn');
+    tabConts[index].classList.add('contentOn');
+  })
+})
 
+// tab click
 tabBtns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     tabBtns.forEach(otherBtn => {
