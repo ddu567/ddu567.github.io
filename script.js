d035204402fa5e34982ed2005d0eef1cb6240ed1
ddu567 <shbea0503@naver.com>
2024-01-30 01:33:45 +0900

    240130
    - 전체적인 디자인 수정
    - 모달 팝업 작성
    - 프로젝트 내용 수정

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Works
 const preview = document.querySelectorAll(".preview > img");
 const tabBtns = document.querySelectorAll(".tabTitle > div");
 const tabConts = document.querySelectorAll('.contentWrap > div');
@@ -30,3 +31,51 @@ tabBtns.forEach((btn, index) => {
   });
 });
 
+
+
+// modal popup
+const popup = document.querySelectorAll('.popup');
+const modalBg = document.querySelector('.modalWrap');
+const modal = document.querySelector('.modal');
+const contentTitle = document.querySelectorAll('.modalHead > li')
+const contentList = document.querySelectorAll('.modalContent > li')
+const close = document.querySelector('.modal span');
+
+function closeFn() {
+  modal.classList.remove('modalOn');
+  modalBg.classList.remove('bg');
+  contentTitle.forEach((title) => {
+    title.style.display = 'none';
+  });
+  contentList.forEach((list) => {
+    list.style.display = 'none';
+  });
+}
+
+popup.forEach((pop, index) => {
+  pop.style.cursor = 'pointer';
+  pop.addEventListener('click', () => {
+    closeFn();
+    modalBg.classList.add('bg');
+    modal.classList.add('modalOn');
+    contentTitle[index].style.display = 'block';
+    contentList[index].style.display = 'block';
+  });
+});
+
+// close modal
+close.style.cursor = 'pointer';
+close.addEventListener('click', () => {
+  closeFn();
+  // contentList.forEach((list) => {
+  //   list.classList.remove('modalOn');
+  // });
+});
+
+// esc close modal
+window.addEventListener("keyup", e => {
+  e.preventDefault();
+  if (e.key == "Escape") {
+    closeFn();
+  }
+});
